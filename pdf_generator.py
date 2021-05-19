db4751ce527366c62c1616a923e69e69abffe9eb
Vineet <vineet@nanotech-softapp.com>
2021-05-19 16:53:19 +0530

    changes

--- pdf_generator.py
+++ pdf_generator.py
@@ -49,7 +49,7 @@ def editPDF(data_dict):
     output_pdf.addPage(page)
     letters = string.digits
     file_name_random = ''.join(random.choice(letters) for i in range(5))
-    file_name = f"./digital_stratahedron_{file_name_random}.pdf"
+    file_name = f"./static/pdf/user_pdf/digital_stratahedron_{file_name_random}.pdf"
     outputStream = open(file_name, "wb")
     output_pdf.write(outputStream)
     outputStream.close()
